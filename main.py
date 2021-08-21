96e368889e00f53607427a05167e0df75463cd6f
panchocorderos <fco.cordero98@gmail.com>
2021-08-21 14:18:33 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+class Person:
+  def __init__(self, name, bday, isAthlete, gender, dateWeight, weight, height):
+        self.name = name #name person
+        self.bday = bday #birth day date
+        self.isAthlete = isAthlete #bool physical condition
+        self.gender = gender #gender (man or woman)
+        self.dateWeight = dateWeight #date weigh
+        self.weight = weight # weight in kg
+        self.height = height #height in mt
+
+
+
+
+# if __name__ == "__main__":
+
+# Requirements
+# age >= 15 and age <= 70
+# imc = weight/height**2
+
