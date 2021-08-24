d1873dbec5cd91cc1b49b31c78619d958021c3b4
yurehn <d.carrascoquinchn@uandresbello.edu>
2021-08-24 13:14:05 -0400

    validaciones completadas

--- main.py
+++ main.py
@@ -33,58 +33,105 @@ class Person:
 
 
 def validarNombre(name):
-
   # Acepta solo letras, espacios y tildes
   regex = re.compile(r"^[a-zA-Z\u00C0-\u017F\s]+$", re.U)
-  print(name, regex.match(name) is not None)
+  # print(name, regex.match(name) is not None)
+  return regex.match(name) is not None
 
 
 def validarIsAthlete(Athlete):
   # Acepta solo si y no
-  regex = re.compile(r'^(?:si|no)$', re.U)
-  print(Athlete, regex.match(Athlete) is not None)
+  regex = re.compile(r'^(?:SI|NO)$', re.U)
+  # print(Athlete, regex.match(Athlete) is not None)
+  return regex.match(Athlete) is not None
 
 def validarGender(gender):
 
   # Acepta solo M y F
-  regex = re.compile(r'^(?:m|f)$', re.U)
-  print(gender, regex.match(gender) is not None)
+  regex = re.compile(r'^(?:M|F)$', re.U)
+  # print(gender, regex.match(gender) is not None)
+  return regex.match(gender) is not None
 
-def validarNumero(num):
-  # Acepta solo numeros
-  # regex = re.compile(r'^(\d{1})([.]?[0-9]{0,3})$', re.U)
 
+def validarheight(num):
+  # Acepta solo numeros 1 | 1.1 | 1.12 | 1.123
+  regex = re.compile(r'^(\d{1})([.]\d{1,3})?$', re.U)
   # print(num, regex.match(num) is not None)
-  pass
+  return regex.match(num) is not None
+
+def validarWeight(weight):
+  # Acepta solo numeros 1 | 12 | 135
+  regex = re.compile(r'^(\d{1,3})([.]\d{1,2})?$', re.U)
+  # print(weight, regex.match(weight) is not None)
+  return regex.match(weight) is not None
 
 def calcularEdad(fecha):
   dateNow = datetime.now()
-  objetoFecha = datetime.strptime(fecha, '%d/%m/%Y') # Trasforma la fecha ingresada en objeto dateTime
-  edad = dateNow.year - objetoFecha.year - ((dateNow.month, dateNow.day) < (objetoFecha.month, objetoFecha.day)) # Tambien se puede relativedelta
 
-  return True if(edad >= 15 and edad <= 70)else(False)
+  try:
+    objetoFecha = datetime.strptime(fecha, '%d/%m/%Y') # Trasforma la fecha ingresada en objeto dateTime
+    edad = dateNow.year - objetoFecha.year - ((dateNow.month, dateNow.day) < (objetoFecha.month, objetoFecha.day)) # Tambien se puede relativedelta
+  except:
+    # raise TypeError("Formato ingresado es invalido")
+    print("Error Formato ingresado es invalido")
+    return "Error"
+  else:
+    return True if(edad >= 15 and edad <= 70)else(False)
+
 
 if __name__ == "__main__":
 
   print("------------- APP IMC -------------")
   name = input("|  Ingrese nombre: ")
+  nameValidar = validarNombre(name)
+  while not nameValidar:
+    print("Error nombre invalido")
+    name = input("|  Ingrese nombre: ")
+    nameValidar = validarNombre(name)
+
 
   bday = input("|  Ingrese Fecha de nacimiento[24/12/1999]: ")
   edad = calcularEdad(bday) # entre 1950 a 2005
+  while (not edad) or (edad == "Error"):
+    if not edad:
+      print("      -------- Error --------")
+      print("      Debe tener entre 15 a 70 años")
+      bday = input("|  Ingrese Fecha de nacimiento[24/12/1999]: ")
+      edad = calcularEdad(bday)  # entre 1950 a 2005
+    else:
+      bday = input("|  Ingrese Fecha de nacimiento[24/12/1999]: ")
+      edad = calcularEdad(bday)
 
-  while not edad:
-    print("      -------- Error --------")
-    print("      Debe tener entre 15 a 70 años")
-    bday = input("|  Ingrese Fecha de nacimiento[24/12/1999]: ")
-    edad = calcularEdad(bday)  # entre 1950 a 2005
+  isAthlete = input("|  ¿Hace ejercicio?[Si/No]: ").strip().upper()
+  isAthleteValidar = validarIsAthlete(isAthlete)
+  while not isAthleteValidar:
+    print("Error: Solo es valido [Si / No]")
+    isAthlete = input("|  ¿Hace ejercicio?[Si/No]: ").strip().upper()
+    isAthleteValidar = validarIsAthlete(isAthlete)
 
-  isAthlete = input("|  ¿Hace ejercicio?[Si/No]: ").strip().lower()
 
-  gender = input("|  Genero [M/F]: ").strip().lower()
+  gender = input("|  Genero [M/F]: ").strip().upper()
+  genderValidar = validarGender(gender)
+  while not genderValidar:
+    print("Error: Solo es valido [M / F]")
+    gender = input("|  Genero [M/F]: ").strip().upper()
+    genderValidar = validarGender(gender)
+
 
   weight = input("|  Peso[K]: ").strip().replace(",", ".")
-  height = input("|  Altura[M]: ").strip().replace(",", ".")
+  weightValidar = validarWeight(weight)
+  while not weightValidar:
+    print("Error: solo es valido hasta tres digitos o hasta tres digitos y dos decimales")
+    weight = input("|  Peso[K]: ").strip().replace(",", ".")
+    weightValidar = validarWeight(weight)
 
+
+  height = input("|  Altura[M]: ").strip().replace(",", ".")
+  heightValidar = validarheight(height)
+  while not heightValidar:
+    print("Error: Solo es valido un digito o un digito y hasta 3 decimales")
+    height = input("|  Altura[M]: ").strip().replace(",", ".")
+    heightValidar = validarheight(height)
 
   dateNow = datetime.now()
 
