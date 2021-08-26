131d2ddc7121bcd720d7bf33b671d3c4e3509961
yurehn <d.carrascoquinchn@uandresbello.edu>
2021-08-26 13:24:51 -0400

    se creo menu y nuevas validaciones

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 import re
 
-# Requirements
-# age >= 15 and age <= 70
-# imc = weight/height**2
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
 class Person:
-  def __init__(self, name, bday, isAthlete, gender, dateWeight, weight, height):
+  def __init__(self, name, bday, age, isAthlete, gender, dateWeight, weight, height):
         self.name = name #name person
         self.bday = bday #birth day date
+        self.age = age
         self.isAthlete = isAthlete #bool physical condition
         self.gender = gender #gender (man or woman)
         self.dateWeight = dateWeight #date weigh
@@ -48,20 +48,24 @@ class Person:
 
 
   def showInfo(self):
+    print("\n\n")
     print("-------- Info --------")
     print("|")
     print("|  Nombre: "+self.name)
+    print("|  edad: "+self.age)
     print("|  Fecha Nacimiento: "+self.bday)
     print("|  ¿Hace ejercicio?: "+self.isAthlete)
     print("|  Genero: "+self.gender)
-    print("|  Peso: "+self.weight)
-    print("|  Altura: "+self.height)
+    print("|  Peso: "+self.weight + " Kg")
+    print("|  Altura: "+self.height + " M")
     print("|  IMC: "+str(self.imc))
     print("|  Interpretacion: "+self.interpretation)
     print("|")
     print("|  Fecha Registro: "+self.dateWeight)
     print("|")
     print("----------------------")
+    print("\n")
+
 
 
 
@@ -108,42 +112,43 @@ def calcularEdad(fecha):
     edad = dateNow.year - objetoFecha.year - ((dateNow.month, dateNow.day) < (objetoFecha.month, objetoFecha.day)) # Tambien se puede relativedelta
   except:
     # raise TypeError("Formato ingresado es invalido")
-    print("Error Formato ingresado es invalido")
+
     return "Error"
   else:
-    return True if(edad >= 15 and edad <= 70)else(False)
+    return [True, edad] if(edad >= 15 and edad <= 70)else(False)
 
 
-if __name__ == "__main__":
 
-  print("------------- APP IMC -------------")
+
+
+def ingresarDatos():
+  print("------------- Ingreso -------------")
   name = input("|  Ingrese nombre: ")
   nameValidar = validarNombre(name)
   while not nameValidar:
-    print("Error nombre invalido")
+    print("Error nombre invalido, solo se aceptan letras")
     name = input("|  Ingrese nombre: ")
     nameValidar = validarNombre(name)
 
-
   bday = input("|  Ingrese Fecha de nacimiento[24/12/1999]: ")
   edad = calcularEdad(bday) # entre 1950 a 2005
-  while (not edad) or (edad == "Error"):
-    if not edad:
+  while (edad == "Error") or (not edad[0]):
+    if not edad[0]:
       print("      -------- Error --------")
       print("      Debe tener entre 15 a 70 años")
       bday = input("|  Ingrese Fecha de nacimiento[24/12/1999]: ")
       edad = calcularEdad(bday)  # entre 1950 a 2005
     else:
+      print("Error Formato ingresado es invalido")
       bday = input("|  Ingrese Fecha de nacimiento[24/12/1999]: ")
       edad = calcularEdad(bday)
 
   isAthlete = input("|  ¿Hace ejercicio?[Si/No]: ").strip().upper()
   isAthleteValidar = validarIsAthlete(isAthlete)
   while not isAthleteValidar:
-    print("Error: Solo es valido [Si / No]")
+    print("\nError: Solo es valido [Si / No]\n")
     isAthlete = input("|  ¿Hace ejercicio?[Si/No]: ").strip().upper()
     isAthleteValidar = validarIsAthlete(isAthlete)
-
 
   gender = input("|  Genero [M/F]: ").strip().upper()
   genderValidar = validarGender(gender)
@@ -151,29 +156,95 @@ if __name__ == "__main__":
     print("Error: Solo es valido [M / F]")
     gender = input("|  Genero [M/F]: ").strip().upper()
     genderValidar = validarGender(gender)
+  if gender == "M":
+    gender = "Masculino"
+  else:
+    gender = "Femenino"
 
-
-  weight = input("|  Peso[K]: ").strip().replace(",", ".")
+  weight = input("|  Peso[Kg]: ").strip().replace(",", ".")
   weightValidar = validarWeight(weight)
-  while not weightValidar:
-    print("Error: solo es valido hasta tres digitos o hasta tres digitos y dos decimales")
-    weight = input("|  Peso[K]: ").strip().replace(",", ".")
-    weightValidar = validarWeight(weight)
+  while (not weightValidar) or (float(weight) == 0):
 
+    if float(weight) == 0:
+      print("\nError: no se puede ingresar 0\n")
+      weight = input("|  Peso[K]: ").strip().replace(",", ".")
+      weightValidar = validarWeight(weight)
+    else:
+      print("Error: solo es valido hasta tres digitos o hasta tres digitos y dos decimales")
+      weight = input("|  Peso[K]: ").strip().replace(",", ".")
+      weightValidar = validarWeight(weight)
 
   height = input("|  Altura[M]: ").strip().replace(",", ".")
   heightValidar = validarheight(height)
-  while not heightValidar:
-    print("Error: Solo es valido un digito o un digito y hasta 3 decimales")
-    height = input("|  Altura[M]: ").strip().replace(",", ".")
-    heightValidar = validarheight(height)
+  while (not heightValidar) or (float(height) == 0):
+    print(height)
+    if float(height) == 0:
+      print("\nError: no se puede ingresar 0\n")
+      height = input("|  Altura[M]: ").strip().replace(",", ".")
+      heightValidar = validarheight(height)
+    else:
+      print("Error: Solo es valido un digito o un digito y hasta 3 decimales")
+      height = input("|  Altura[M]: ").strip().replace(",", ".")
+      heightValidar = validarheight(height)
 
+
+  print("|")
+  print("----------------------")
   dateNow = datetime.now().strftime("%d/%m/%Y")
+  print(edad)
+  return Person(name, bday, str(edad[1]), isAthlete, gender, str(dateNow), weight, height)
 
-  PruebaUno = Person(name, bday, isAthlete, gender, str(dateNow), weight, height)
-  PruebaUno.getIMC()
-  PruebaUno.getInterpretation()
-  PruebaUno.showInfo()
+def buscarDatos():
+  pass
 
-  # PruebaDos = Person("Daniel", "24/12/1999", "N", "M", "22/08/2021", "50", "2")
-  # PruebaDos.showInfo()
+
+if __name__ == "__main__":
+
+  storagePerson = []
+
+  ciclo = True
+  while ciclo:
+
+    print("------------- APP IMC -------------")
+    print("|")
+    print("|  | 1 | Ingresar")
+    print("|  | 2 | Buscar")
+    print("|  | 3 | Salir")
+    print("|")
+    opcion = input("|  Ingrese la opcion: ")
+    print("|")
+    print("----------------------")
+    print("\n")
+
+    if opcion == "1":
+      # dateNow = datetime.now().strftime("%d/%m/%Y")
+      # nuevoIngreso = Person("Daniel", "24/12/1999", "N", "Masculino", dateNow, "50", "2")
+      # nuevoIngreso.getIMC()
+      # nuevoIngreso.getInterpretation()
+      # nuevoIngreso.showInfo()
+      # storagePerson.append(nuevoIngreso)
+
+      nuevoIngreso = ingresarDatos()
+      nuevoIngreso.getIMC()
+      nuevoIngreso.getInterpretation()
+      nuevoIngreso.showInfo()
+      storagePerson.append(nuevoIngreso)
+    elif opcion == "2":
+
+      nameSearch = input("|  Ingrese nombre a buscar: ")
+      nameValidar = validarNombre(nameSearch)
+      if not nameValidar:
+        print("\nError nombre invalido, solo se aceptan letras\n")
+      else:
+        objetoUsuario = list(filter(lambda objeto: objeto.name == nameSearch,storagePerson))
+
+        if len(objetoUsuario) != 0:
+          for i in objetoUsuario:
+            i.showInfo()
+        else:
+          print("\n   Usuario no encontrado\n")
+
+    elif opcion == "3":
+      ciclo = False
+    else:
+      print("  Opcion Invalida")
